4a3d671e91f49e7c193933b1357fb2f3230e5ff6
Michael Ly <17331561+codeaftermath@users.noreply.github.com>
2024-12-07 13:16:39 -0800

    Include workaround to setting credentials for AWS EKS environment

--- udagram-api-feed/src/aws.ts
+++ udagram-api-feed/src/aws.ts
@@ -1,25 +1,39 @@
 import AWS = require('aws-sdk');
 import {config} from './config/config';
 
-
 // Configure AWS
-AWS.CredentialProviderChain.defaultProviders = [
-  function () { return new AWS.EnvironmentCredentials('AWS'); },
-  function () { return new AWS.SharedIniFileCredentials({profile: config.aws_profile }); }
-];
-new AWS.CredentialProviderChain().resolve((err, credential) => {
-  if (!err) {
-    AWS.config.credentials = credential
-  }
-  else {
-    console.error("Unable to set AWS credential", err)
-  }
-});
+const accessKeyId = process.env.AWS_ACCESS_KEY_ID ? process.env.AWS_ACCESS_KEY_ID.trim() : undefined;
+const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY ? process.env.AWS_SECRET_ACCESS_KEY.trim() : undefined;
+const sessionToken = process.env.AWS_SESSION_TOKEN ? process.env.AWS_SESSION_TOKEN.trim() : undefined;
+if (accessKeyId && secretAccessKey) {
+  // Workaround to mitigate line feed character being added that causes SignatureDoesNotMatch
+  AWS.config.credentials = 
+    new AWS.Credentials({
+      accessKeyId: accessKeyId,
+      secretAccessKey: secretAccessKey,
+      sessionToken: sessionToken,
+    });
+  AWS.config.region = config.aws_region.trim();
+}
+else {
+  new AWS.CredentialProviderChain().resolve((err, credential) => {
+    if (!err) {
+      // NOTE: Discovered that \n is being added in the process somewhere. This causes the issue SignatureDoesNotMatch
+      // Related to Kubernetes secret values are being handled. Noticed that new line feed were added to all values which gets included to aws-sdk
+      // console.log(credential); 
+  
+      AWS.config.credentials = credential
+      AWS.config.region = config.aws_region
+    }
+    else {
+      console.error("Unable to set AWS credential", err)
+    }
+  });
+}
 
 export const s3 = new AWS.S3({
   signatureVersion: 'v4',
-  region: config.aws_region,
-  params: {Bucket: config.aws_media_bucket},
+  params: {Bucket: config.aws_media_bucket.trim()},
 });
 
 // Generates an AWS signed URL for retrieving objects
@@ -27,8 +41,8 @@ export function getGetSignedUrl( key: string ): string {
   const signedUrlExpireSeconds = 60 * 5;
 
   return s3.getSignedUrl('getObject', {
-    Bucket: config.aws_media_bucket,
-    Key: key,
+    Bucket: config.aws_media_bucket.trim(),
+    Key: key.trim(),
     Expires: signedUrlExpireSeconds,
   });
 }
