1bb11bc1e4d9df3a8f0011c2fc83050a15fa0df5
Michael Ly <17331561+codeaftermath@users.noreply.github.com>
2024-12-08 12:03:10 -0800

    Update template script

--- ops/deploy/env/apply.sh
+++ ops/deploy/env/apply.sh
@@ -4,6 +4,6 @@ set -x -e -u -o pipefail
 
 kubectl version
 
-kubectl apply -f ./aws-secret.yaml
-kubectl apply -f ./env-secret.yaml
-kubectl apply -f ./env-configmap.yaml
+kubectl apply -f ./aws-secret.yml
+kubectl apply -f ./env-secret.yml
+kubectl apply -f ./env-configmap.yml
